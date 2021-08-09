24725da1b5e5045c3829386cc7e2f6825d8ed57b
jameshhe <jameshuang@smu.edu>
2021-08-09 15:04:47 -0500

    added styling to code upload page

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -2,43 +2,63 @@ import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom"
 import {API} from "aws-amplify"
 import {getCode} from '../graphql/queries';
-import { CodeBlock, dracula } from "react-code-blocks";
+// import { CodeBlock, nord } from "react-code-blocks";
+import Editor from "react-simple-code-editor";
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/themes/prism.css"; //Example style, you can use another
 
 const Code = () => {
 
     let {id} = useParams();
 
-    const [code, setCode] = useState([]);
+    // const [code, setCode] = useState([]);
+    const [code, setCode] = useState(
+        `function add(a, b) { return a + b;\n}`
+      );
 
-    useEffect(() => {
-        fetchCode()
-    }, [])
+    // useEffect(() => {
+    //     fetchCode()
+    // }, [])
 
-    const fetchCode = async () => {
-        const codeData = await API.graphql({query: getCode, variables: {id: id}})
-        console.log(codeData)
-        setCode(codeData.data.getCode)
-    }
+    // const fetchCode = async () => {
+    //     const codeData = await API.graphql({query: getCode, variables: {id: id}})
+    //     console.log(codeData)
+    //     setCode(codeData.data.getCode)
+    // }
     
     return (
         <div>
-            <h1>{code.number}. {code.problem}</h1>
+            {/* <h1>{code.number}. {code.problem}</h1>
             <p className="code-description">{code.description}</p>
-            <p className="code-description">Beauty: {code.beauty} </p>
+            <p className="code-description">Beauty: {code.beauty} </p> */}
             {/* <pre class="line-numbers">
                 <code class="language-css">
                     {code.solution}
                 </code>
             </pre> */}
-            <div className="container mx-auto p-4">
+            {/* <div className="container mx-auto p-4">
                 <CodeBlock
                     language={"python"}
                     text={code.solution}
-                    theme={dracula}
+                    theme={nord}
                     wrapLines={true}
                     codeBlock
                 />
-            </div>
+            </div> */}
+            <Editor
+                value={code}
+                onValueChange={(code) => setCode(code)}
+                highlight={(code) => highlight(code, languages.js)}
+                padding={10}
+                style={{
+                    fontFamily: '"Fira code", "Fira Mono", monospace',
+                    fontSize: 12,
+                }}
+                className="wrapline"
+            />
+            {console.log(code)}
             
         </div>
     );
--- src/components/CodeUpload.jsx
+++ src/components/CodeUpload.jsx
@@ -1,81 +1,100 @@
-import React, {useState} from 'react';
-import {API, graphqlOperation} from "aws-amplify";
-import {createCode} from "../graphql/mutations"
+import React, { useState } from "react";
+import { API, graphqlOperation } from "aws-amplify";
+import { createCode } from "../graphql/mutations";
 
 const CodeUpload = () => {
-    const initialForm = {
-        problem: "",
-        solution: "",
-        beauty: "",
-        description: ""
-    }
+  const initialForm = {
+    problem: "",
+    solution: "",
+    beauty: "",
+    description: "",
+  };
 
-    const [data, setData] = useState(initialForm)
+  const [data, setData] = useState(initialForm);
 
-    const handleSubmit = async () => {
+  const handleSubmit = async () => {
+    let codeDetails = {
+      number: 1,
+      problem: data.problem,
+      abbreviation: data.problem,
+      solution: data.solution,
+      beauty: data.beauty,
+      description: data.description,
+    };
+    console.log(codeDetails);
+    await API.graphql(graphqlOperation(createCode, { input: codeDetails }));
+    setData(initialForm);
+  };
 
-        let codeDetails = {
-            number: 1,
-            problem: data.problem,
-            abbreviation: data.problem,
-            solution: data.solution,
-            beauty: data.beauty,
-            description: data.description
-        }
-        console.log(codeDetails)
-        await API.graphql(graphqlOperation(createCode, {input: codeDetails}))
-        setData(initialForm)
-    }
+  return (
+    <div>
+      <h1>Create a new problem</h1>
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <h1>Create a new problem</h1>
-
-            <label>
-            Problem:
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label className="form-label">
+            Problem
             <input
-                name="problem"
-                type="text"
-                value={data.problem}
-                onChange={e => setData({...data, "problem": e.target.value})}
-                required />
-            </label>
+              name="problem"
+              type="text"
+              value={data.problem}
+              onChange={(e) => setData({ ...data, problem: e.target.value })}
+              className="form-control"
+              required
+            />
+          </label>
+        </div>
 
-            <label>
-            Solution:
+        <div className="mb-3">
+          <label className="form-label">
+            Solution
             <input
-                name="solution"
-                type="text"
-                value={data.solution}
-                onChange={e => setData({...data, "solution": e.target.value})}
-                required />
-            </label>
+              name="solution"
+              type="text"
+              value={data.solution}
+              onChange={(e) => setData({ ...data, solution: e.target.value })}
+              className="form-control"
+              required
+            />
+          </label>
+        </div>
 
-            <label>
-            Description:
-            <input
-                name="description"
-                type="text"
-                value={data.description}
-                onChange={e => setData({...data, "description": e.target.value})}
-                required />
-            </label>
+        <div className="mb-3">
+          <label className="form-label">
+            Description
+            <textarea
+              name="description"
+              type="text"
+              value={data.description}
+              onChange={(e) =>
+                setData({ ...data, description: e.target.value })
+              }
+              className="form-control"
+              rows="3"
+              required
+            />
+          </label>
+        </div>
 
-            <label>
-            Beauty:
-            <input
-                name="beauty"
-                type="text"
-                value={data.beauty}
-                onChange={e => setData({...data, "beauty": e.target.value})}
-                required />
-            </label>
+        <div className="mb-3">
+          <label className="form-label">
+            Beauty
+            <textarea
+              name="beauty"
+              type="text"
+              value={data.beauty}
+              onChange={(e) => setData({ ...data, beauty: e.target.value })}
+              className="form-control"
+              rows="3"
+              required
+            />
+          </label>
+        </div>
 
-            
-
-            <button>Submit</button>
-        </form>
-    );
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default CodeUpload;
